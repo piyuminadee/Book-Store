a0b13bcd2dbd308bb4a1dca037ad667dda808743
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2024-07-03 23:07:23 +0530

    changes

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -27,6 +27,20 @@ const ShowBook = () => {
     <div className='p-4'>
     <BackButton/>
     <h1 className='text-3xl my-4'>Show Book</h1>
+    {loading ? (
+      <Spinner />
+    ) : (
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
+        <div className='my-4'>
+        <span className='text-xl mr-4 text-ray-500'>Id</span>
+        <span>{book._id}</span>
+        <span className='text-xl mr-4 text-ray-500'>Title</span>
+        <span className='text-xl mr-4 text-ray-500'>Id</span>
+
+
+        </div>
+      </div>
+    )}
     </div>
   )
 }
