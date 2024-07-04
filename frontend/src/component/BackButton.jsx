b1afddb8edf5563c7840bd781140588b4fd3b8e4
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2024-07-04 16:45:42 +0530

    implement showbook button

--- frontend/src/component/BackButton.jsx
+++ frontend/src/component/BackButton.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import {Link} from 'react-router-dom';
-import {BsArrowLeft} from 'react-icons/bs';
+import { Link } from 'react-router-dom';
+import { BsArrowLeft } from 'react-icons/bs';
 
-
-export const BackButton = ({ destination = '/'}) => {
+const BackButton = ({ destination = '/' }) => {
   return (
     <div className='flex'>
-      <Link 
+      <Link
         to={destination}
-        className='bg-sky-800 text-white px-4 py-1  rounded-lg w-fit'
+        className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
       >
         <BsArrowLeft className='text-2xl' />
       </Link>
     </div>
-  )
-}
+  );
+};
+
+export default BackButton;
