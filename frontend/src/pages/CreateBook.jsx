150357c6e53e5d14f64142b33608ed3348027fd9
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2024-07-06 00:29:14 +0530

    update

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BackButton from '../component/BackButton';
+import Spinner from '../component/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function CreateBook() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleSaveBook = () => {
+      const data = {
+        title,
+        author,
+        publishYear,
+      };
+      setLoading(true);
+      axios
+        .post('http://localhost:3000/books/${id}')
+  }
   return (
     <div>CreateBook</div>
   )
